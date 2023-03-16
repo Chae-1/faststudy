e9fed19bde08ad1af07d5300d30e34d2688a3a5c
Chae-1 <coguddlf1000@naver.com>
2023-03-17 00:58:32 +0900

    d

---
+++ src/main/java/chbo/chbo/ch3/AppConf2.java
@@ -0,0 +1,37 @@
+package chbo.chbo.ch3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConf2 {
+    @Autowired
+    private MemberDao memberDao;
+    @Autowired
+    private MemberPrinter memberPrinter;
+
+    @Bean
+    public MemberResisterService resisterService() {
+        return new MemberResisterService(memberDao);
+    }
+
+    @Bean
+    public MemberPrinter memberPrinter() {
+        return new MemberPrinter();
+    }
+
+    @Bean
+    public MemberListPrinter memberListPrinter() {
+        return new MemberListPrinter(memberDao, memberPrinter);
+    }
+
+    @Bean
+    public MemberInfoPrinter memberInfoPrinter() {
+        MemberInfoPrinter memberInfoPrinter = new MemberInfoPrinter();
+        memberInfoPrinter.setMemberDao(memberDao);
+        memberInfoPrinter.setPrinter(memberPrinter);
+        return memberInfoPrinter;
+    }
+
+}
