92aa7f6eecc4594a49a54889d30ba6b4595187cd
Chae-1 <coguddlf1000@naver.com>
2023-03-17 01:08:01 +0900

    dd

--- src/main/java/chbo/chbo/ch3/AppCtx.java
+++ src/main/java/chbo/chbo/ch3/AppCtx.java
@@ -11,33 +11,8 @@ public class AppCtx {
     }
 
     @Bean
-    public ChangePasswordService changePasswordService() {
-        ChangePasswordService changePasswordService = new ChangePasswordService();
-        changePasswordService.setMemberDao(memberDao());
-        return changePasswordService;
-
-    }
-
-    @Bean
-    public MemberResisterService resisterService() {
-        return new MemberResisterService(memberDao());
-    }
-
-    @Bean
     public MemberPrinter memberPrinter() {
         return new MemberPrinter();
     }
 
-    @Bean
-    public MemberListPrinter memberListPrinter() {
-        return new MemberListPrinter(memberDao(), memberPrinter());
-    }
-
-    @Bean
-    public MemberInfoPrinter memberInfoPrinter() {
-        MemberInfoPrinter memberInfoPrinter = new MemberInfoPrinter();
-        memberInfoPrinter.setMemberDao(memberDao());
-        memberInfoPrinter.setPrinter(memberPrinter());
-        return memberInfoPrinter;
-    }
 }
